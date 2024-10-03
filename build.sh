36fcff62410d27f809af21c00ffab96d50976a95
Heiko Jakubzik <heiko.jakubzik@shj-online.de>
2024-10-03 19:48:09 +0200

    First working release

---
+++ build.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#
+# ======================================
+# EDIT
+PROGRAMMVERSION="0.0.1" 
+MSG="First working release" # COMMIT MSG FOR GIT
+# ======================================
+
+function check_test_outcome {
+  ret_code=$1
+  if [ $ret_code -ne 0 ] ; then
+    echo -e "\e[31mFAIL: $ret_code\e[0m";
+    exit 1;
+  else
+    echo -e "\e[32mok\e[0m"
+  fi;
+}
+
+ORDNER="/home/heiko/development/rust/rremind"
+AUR_ORDNER="/home/heiko/tools/rremind_upstream"
+DATUM=$(date '+%B %d, %Y')
+TMPFOLDER="/tmp/rremind"
+MANFILE="./rremind.1.gz"
+
+echo "BUILD: Setting dates and version (to $DATUM and $PROGRAMMVERSION)..."
+sed -e "s/#PROGRAMMVERSION#/$PROGRAMMVERSION/g" "$ORDNER/manpage_template.md" > "$ORDNER/manpage.md"
+sed -i -e "s/#DATUM#/$DATUM/g" "$ORDNER/manpage.md" 
+sed -e "s/#PROGRAMMVERSION#/$PROGRAMMVERSION/g" "$ORDNER/Cargo_template.toml" > "$ORDNER/Cargo.toml"
+echo "BUILD: ...set."
+echo ""
+echo "BUILD: Compiling manpage..."
+rm $MANFILE
+pandoc ./manpage.md -s -t man -o ./rremind.1
+gzip ./rremind.1
+check_test_outcome
+echo "BUILD: ...compiled"
+
+echo "BUILD: Compiling binary for AUR with -m..."
+cargo-aur -m b
+check_test_outcome
+echo "BUILD: ...compiled."
+
+PGV="rremind-$PROGRAMMVERSION-x86_64.tar.gz"
+echo "BUILD: Producing binary $PGV with manpage inside..."
+rm -rf $TMPFOLDER
+mkdir -p "$TMPFOLDER"
+cp "$ORDNER/target/cargo-aur/$PGV" "$TMPFOLDER/"
+cp "$ORDNER/target/cargo-aur/LICENSE.md" "$TMPFOLDER/"
+cp "$ORDNER/rremind.1.gz" "$TMPFOLDER/"
+cd $TMPFOLDER
+tar -xf "$PGV"
+rm "$PGV"
+tar -czf $PGV rremind LICENSE.md rremind.1.gz
+cp $PGV "$ORDNER/target/cargo-aur/"
+cp $PGV ~/tools/rremind_upstream/
+echo "BUILD: ...produced."
+
+cd "$ORDNER"
+echo "BUILD: Updating GIT..."
+git add .
+check_test_outcome
+git commit -m "$MSG"
+check_test_outcome
+git push origin
+check_test_outcome
+# gh release create v"$PROGRAMMVERSION" --notes "$MSG" "$ORDNER/target/cargo-aur/$PGV"
+echo "BUILD: ...committed"
+
+SHASUM=$(sha256sum  "$AUR_ORDNER/$PGV" | awk '{print $1}')
+
+sed -e "s/#SHASUM#/$SHASUM/g" "$AUR_ORDNER/SRCINFO_template.md" > "$AUR_ORDNER/rremind/.SRCINFO"
+sed -i -e "s/#PROGRAMMVERSION#/$PROGRAMMVERSION/g" "$AUR_ORDNER/rremind/.SRCINFO" 
+
+sed -e "s/#SHASUM#/$SHASUM/g" "$AUR_ORDNER/PKGBUILD_template.md" > "$AUR_ORDNER/rremind/PKGBUILD"
+sed -i -e "s/#PROGRAMMVERSION#/$PROGRAMMVERSION/g" "$AUR_ORDNER/rremind/PKGBUILD" 
+
+echo "BUILD: Going to $AUR_ORDNER, pushing commit there..."
+cd "$AUR_ORDNER/rremind"
+git add .
+check_test_outcome
+git commit -m "$MSG"
+check_test_outcome
+git push
+check_test_outcome
+echo "BUILD: ...pushed"
+
+gh release create v"$PROGRAMMVERSION" "$ORDNER/target/cargo-aur/$PGV"
+check_test_outcome
+#
+
+echo "BUILD: Publishing on crates.io?"
+cd "$ORDNER"
+cargo publish
+echo "BUILD: finished"
---
+++ manpage.md
@@ -0,0 +1,55 @@
+---
+title: RREMIND
+section: 1
+header: User Manual
+footer: rremind #RREMINDVERSION#
+date: Oktober 03, 2024
+---
+# NAME
+
+rremind -- a reminder service inspired by Diane Skoll's remind
+
+# SYNOPSIS
+
+**rremind** [*OPTION*]
+
+# DESCRIPTION
+
+**rremind** reads through all *.rem files in the configuration folder (which is specified in rremind's first run and then stored in ~/.config/rremind/rr.rc).
+
+Without parameter, "rremind" will list today's appointments.
+
+With an integer parameter i, "rremind" will list the appointments i days relative to today. (`rremind -1` will show yesterday's appointments, `rremind 2` will list the appointments on the day after tomorrow).
+
+With the parameter "when" plus a search string, rremind will list the appointments whose description contains the search string (`rremind when dentist` will list the appointments that contain the word "dentist").
+
+# OPTIONS
+
+**i**, 
+: list appointments relative to today (i is an integer, e.g. -2 for the day before yesterday)
+
+# EXAMPLES
+
+**rremind**
+: list all of today's appointments
+
+**rremind 3**
+: list all appointments three days from today
+
+**rremind -1**
+: list all of yesterday's appointments
+
+**rremind when dendist**
+: list appointments that contain the word "dentist"
+
+# AUTHORS
+
+Written by Heiko Jakubzik, <heiko.jakubzik@shj-online.de>
+
+# BUGS
+
+Submit bug reports online at: <https://github.com/Jakubzik/rremind>.
+
+# SEE ALSO
+
+Full documentation and sources at: <https://github.com/Jakubzik/rremind>.
